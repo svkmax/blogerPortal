ff4d3dfaa7003433ef5c2441135634c2365a5cf4
maxim.malgin <svkmax@gmail.com>
2016-03-17 18:15:40 +0200

    complex content save and create

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,6 +2,7 @@ class Content
   include NoBrainer::Document
   include NoBrainer::Document::Timestamps
 
+
   field :title, type: String
   field :description, type: Text
   field :content_image_url, type: String
@@ -11,5 +12,22 @@ class Content
 
 
   validates :title, :description, :presence => true
+
+
+  def self.create obj_hash
+    content = super obj_hash
+    user = User.find(content.user_id)
+    user.update(contents: user.contents + [content.id]) if content.valid?
+  end
+
+  def save
+    Content.create(self.as_json)
+  end
+
+  def delete
+
+  end
+
+
 
 end
